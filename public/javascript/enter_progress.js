09a855fad423ab37041e0f7b23b39a31510322cd
Kevin Reid <kevinnivek@me.com>
2022-08-13 12:34:59 -0400

    able to wright to results when checkbox is clicked

--- public/javascript/enter_progress.js
+++ public/javascript/enter_progress.js
@@ -1,23 +1,25 @@
 
 
-async function getDateID(dayPassed) {
+async function PostResults(completedStatus, habit, date ) {
 
-    const response = await fetch('/date/:' + dayPassed, {
-      method: 'GET',
-      body: JSON.stringify({
-        date_id,
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert(response.statusText);
+  const response = await fetch('/api/results', {
+    method: 'POST',
+    body: JSON.stringify({
+      is_completed: completedStatus,
+      habit_id: habit,
+      date_id: date
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    console.log("Result Added" + completedStatus + " " + habit + " " + date );
+  } else {
+    alert(response.statusText);
   }
+}
 
 
 // Select all checkboxes with the name 'settings' using querySelectorAll
@@ -34,25 +36,22 @@ checkboxes.forEach(function(checkbox) {
       .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
       .map(i => arrayHabits = (i.id).split("-")) // Use Array.map to extract only the checkbox values from the array of objects then split the value
       
+      // these are for the checked boxes 
       console.log(enabledSettings);
       console.log(arrayHabits);
       let habitChangedID = arrayHabits[0]; // split value for habit given to this variable
       let dayChangedID = arrayHabits[1]; // split value for date given to this variable
       let checboxValue = true;
+      
 
       console.log("The Habit ID changed is " + habitChangedID);
       console.log("the day ID changed is " + dayChangedID);
+      console.log("the value is " + checboxValue);
+  
 
-      // using document query to find the inner html of each table header so we know the date and habit
-      let habitNameChanged = document.querySelector("#" + habitChangedID).innerHTML;
-      let dayNameChanged = document.querySelector("#"+ dayChangedID).innerHTML;
-      console.log("The habit name changed is " + habitNameChanged);
-      console.log("The day name changed is " + dayNameChanged);
-   
-      console.log("checkbox clicked");
+      PostResults(checboxValue, habitChangedID, dayChangedID);
 
-    getDateID(dayNameChanged);
 
-  })
+    })
 });
 
