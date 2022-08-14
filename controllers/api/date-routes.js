903477fd78b799bdbe0592149ec25d3b3f871f5a
Kevin Reid <101418282+Vinyldude8896@users.noreply.github.com>
2022-08-13 21:52:16 -0400

    Revert "Table/get post"

--- controllers/api/date-routes.js
+++ controllers/api/date-routes.js
@@ -2,82 +2,23 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Habit, User, Result, Date } = require('../../models');
 
-// commented to see if I can find all dates
-//find all habits of a specific user 
-// router.get('/', (req, res) => {
-//     Habit.findAll({
-//         where: {
-//             user_id: req.session.user_id
-//         },
-//         attributes: ['id', 'habit_title', 'habit_info', 'user_id'],
-//     })
-//         .then(dbHabitData => {
-//             if (!dbHabitData) {
-//                 res.status(404).json({ message: 'No habits found for this user' });
-//                 return;
-//             }
-//             res.json(dbHabitData);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
-
-
- //find all dates in table
- router.get('/', (req, res) => {
-    // console.log(req.session);
-    console.log('======================');
-        Date.findAll({
-        })
-          .then(dbUserData => res.json(dbUserData))
-          .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-          })
-      });
-
- //find date that matches the ID provided
- router.get('/:id', (req, res) => {
-    Date.findAll({
+ //find all habits of a specific user 
+router.get('/date', (req, res) => {
+    Habit.findAll({
         where: {
-            id: req.params.id
+            user_id: req.session.user_id
         },
-        attributes: ['id', 'date', 'created_at', 'updated_at'],
+        attributes: ['id', 'habit_title', 'habit_info', 'user_id'],
     })
-        .then(dbDateData => {
-            if (!dbDateData) {
-                res.status(404).json({ message: 'No dates found' });
+        .then(dbHabitData => {
+            if (!dbHabitData) {
+                res.status(404).json({ message: 'No habits found for this user' });
                 return;
             }
-            res.json(dbDateData);
+            res.json(dbHabitData);
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
 });
-
- //find date that matches the date string provided
- router.get('/:date', (req, res) => {
-    Date.findAll({
-        where: {
-            date: req.params.date
-        },
-        attributes: ['id', 'date', 'created_at', 'updated_at'],
-    })
-        .then(dbDateData => {
-            if (!dbDateData) {
-                res.status(404).json({ message: 'No dates found' });
-                return;
-            }
-            res.json(dbDateData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-module.exports = router;
