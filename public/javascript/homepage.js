ef37abe39e309f7139a96554cb291697425cde20
rheneadianne <rheneadianne@gmail.com>
2022-08-15 18:19:17 -0400

    finished table ids

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -37,7 +37,6 @@ document.querySelectorAll("input[type=checkbox]").forEach((checkbox) => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          checked: true,
           dateId: +dateId,
           habitId: +habitId,
         }),
