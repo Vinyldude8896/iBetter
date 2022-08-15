6a35d16b7e313ba0e234bd1eb7652dc0712d1c62
Katie <katiebutler37@gmail.com>
2022-08-14 22:13:32 -0400

    troubleshooting helpers

--- utils/helpers.js
+++
@@ -1,9 +0,0 @@
-const { registerHelper } = require("handlebars");
-
-// less than
-Handlebars.registerHelper('lt', function( a, b ){
-	var next =  arguments[arguments.length-1];
-	return (a < b) ? next.fn(this) : next.inverse(this);
-});
-
-module.exports = registerHelper;
