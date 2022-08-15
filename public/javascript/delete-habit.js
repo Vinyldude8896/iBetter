1bf6df2a53c0045756050ed9f267b01c54a1dbe6
rheneadianne <rheneadianne@gmail.com>
2022-08-15 01:59:17 -0400

    added delete route

--- public/javascript/delete-habit.js
+++ public/javascript/delete-habit.js
@@ -4,7 +4,7 @@ async function deleteFormHandler(event) {
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
-  const response = await fetch(`/api/habit/${id}`, {
+  const response = await fetch(`/api/habits/${id}`, {
     method: 'DELETE'
   });
 
