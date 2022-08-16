b3ed2987a3552f6b31f44311d56f0d2030284ccc
Kevin Reid <kevinnivek@me.com>
2022-08-15 20:40:57 -0400

    adding localhost

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "127.0.0.1",
+      host: "localhost",
       dialect: 'mysql',
       port: 3306
     });
