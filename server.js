26c04304542da82d9399dc443ce92a95ca53f112
Kevin Reid <kevinnivek@me.com>
2022-08-15 20:21:40 -0400

    adding main

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(require("./controllers"));
 
 // sequelize and PORT
-sequelize.sync({ force: true }).then(async () => {
+sequelize.sync({ force: false}).then(async () => {
   // await seedAll();
   app.listen(PORT, () => console.log("Now listening"));
 });
