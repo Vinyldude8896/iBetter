92e2414f1552f36184047442f305924abcf1c5e8
rheneadianne <rheneadianne@gmail.com>
2022-08-16 22:00:54 -0400

    fixing chart feature routes

--- seeds/index.js
+++ seeds/index.js
@@ -5,7 +5,7 @@ const seedResults = require('./results-seeds')
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-    await sequelize.sync({ force: true});
+    await sequelize.sync({ force: false});
     console.log('----------------');
     await seedUsers();
     console.log('----------------');
@@ -21,58 +21,5 @@ const seedAll = async () => {
     process.exit(0);
 };
 
-// seedAll();
+seedAll();
 module.exports = seedAll;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const sequelize = require('../config/connection');
-// const { Date, Habit, Result, User } = require('../models');
-
-// const userSeeds = require('./userSeeds.json');
-
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
-
-//   const users = await User.bulkCreate(userSeeds, {
-//     individualHooks: true,
-//     returning: true,
-//   });
-
-//   for (const { id } of users) {
-//     const newCard = await LibraryCard.create({
-//       reader_id: id,
-//     });
-//   }
-
-//   for (const book of bookSeedData) {
-//     const newBook = await Book.create({
-//       ...book,
-//       // Attach a random reader ID to each book
-//       reader_id: readers[Math.floor(Math.random() * readers.length)].id,
-//     });
-//   }
-
-//   process.exit(0);
-// };
-
-// seedDatabase();
